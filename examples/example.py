80af7cf1754b25b29994c49ce5ae8a033f810fce
Moonrise <70655051+PetitPotiron@users.noreply.github.com>
2021-10-09 12:37:41 +0200

    Delete examples directory

--- examples/example.py
+++
@@ -1,9 +0,0 @@
-import convertio
-import time
-
-client = convertio.Client(token="INSERT_YOUR_TOKEN_HERE")
-id = client.convert_by_filename('source.txt', 'pdf')
-while client.check_conversion(id).step != 'finish':
-    time.sleep(1)
-client.download(id, 'result.pdf')
-client.delete(id)
