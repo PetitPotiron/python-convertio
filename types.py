ced4cd3708a237658caf8c0036408bdd42fb8770
Moonrise <70655051+PetitPotiron@users.noreply.github.com>
2021-10-10 13:48:08 +0200

    Add files via upload

---
+++ types.py
@@ -0,0 +1,51 @@
+from typing import Any, Optional, TypedDict
+
+
+class ConversionPostResult(TypedDict):
+    code: int
+    status: str
+    data: 'ConversionPostResultDictData'
+
+
+class ConversionPostResultDictData(TypedDict):
+    id: str
+    minutes: str
+
+
+class ConversionPostDict(TypedDict, total=False):
+    apikey: str
+    input: str
+    file: str
+    filename: Optional[str]
+    outputformat: str
+    options: Optional['ConversionPostDictOptions']
+
+
+class ConversionPostDictOptions(TypedDict):
+    ocr_enabled: bool
+    ocr_settings: 'ConversionPostDictOptionsOcrSettings'
+
+
+class ConversionPostDictOptionsOcrSettings(TypedDict, total=False):
+    langs: list[str]
+    page_nums: Optional[str]
+
+
+class ConversionStatusResult(TypedDict):
+    code: int
+    status: str
+    data: 'ConversionStatusResultData'
+
+
+class ConversionStatusResultData(TypedDict):
+    id: str
+    step: str
+    step_percent: int
+    minutes: int
+    output: 'ConversionStatusResultDataOutPut'
+
+
+class ConversionStatusResultDataOutPut(TypedDict):
+    url: str
+    size: int
+    files: dict[Any, Any]
